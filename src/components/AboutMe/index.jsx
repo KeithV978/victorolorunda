35a4f61a860bf00cc6ca6c59916b4c4dfac314c9
KeithV978 <victorolorunda6@gmail.com>
2024-07-14 10:22:38 +0100

    All roads lead to the finish line

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -23,20 +23,22 @@ export const AboutMe = () => {
             Hi again. <br /> My name is Victor Olorunda, and i'm a fullstack web
             developer.
           </Paragraph>
-          <br />
+          {/* <br /> */}
           <Paragraph variant="body2">
-            Hi again. My name is Victor Olorunda, and i'm a fullstack web
-            developer
+            I have been freelancing for the most part of my tech career. With
+            two years of experience in Customer Support. I am not entirely self
+            taught but for the most part, I am.
           </Paragraph>
-          <br />
+          {/* <br /> */}
           <Paragraph variant="body2">
-            I may not have all the experience but then, I am no stranger to
-            creativity
+            I have a knack for creating things and making things beautiful.
           </Paragraph>
-          <br />
+          {/* <br /> */}
           <Paragraph variant="body2">
-            Hi again. My name is Victor Olorunda, and i'm a fullstack web
-            developer
+            I have worked and few a few projects that have gone live.
+            <br />
+            My portfolio as well as the other things I have worked on have been
+            designed by myself.
             <br />
             ...
           </Paragraph>
